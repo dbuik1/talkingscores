8de6dbb36a6888b0ca39da4e4fe03a090fb21609
dbuik1 <david.buikisme@gmail.com>
2025-07-13 01:40:12 +0100

    venv updates

--- venv_talkingscores/Scripts/activate.bat
+++ venv_talkingscores/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\dbuik\Documents\talkingscores\venv_talkingscores
+set VIRTUAL_ENV=C:\Users\david\Documents\Talkingscores\talkingscores\venv_talkingscores
 
 if not defined PROMPT set PROMPT=$P$G
 
